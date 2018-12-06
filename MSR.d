9d3a9cb9f672a91f01a7e39665d8242335801662
IMGP3DEV <42507604+Imgp3Dev@users.noreply.github.com>
2018-12-06 13:54:12 -0600

    Create MSR.d

---
+++ MSR.d
@@ -0,0 +1,27 @@
+// 'Encryption' Program.
+// Written in D
+/* MSR(Month shift right) Fun Program for
+ciphering.*/
+Month[string] = {"January", "Febuary",
+"March", "April", "May", "June", "July",
+"August", "September", "October",
+"November", "December"};
+//Month List
+EndLetters[char] = {'y', 'h', 'l', 'e',
+'t', 'r'};
+//Letter List
+writeln("Enter a Text: ");
+//Enter text
+writeln.log = "Input";
+//created "Input" assigned to be var for
+inputs.
+
+if(check.EndLetters...%){
+wrintln();
+/*if it notices any of the endletters then
+it will just have it stay there and not
+change*/
+}else{
+input...%(Month)){
+check.Input(0);
+//Goes 1 to the right
