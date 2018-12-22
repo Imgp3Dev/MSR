4edf6e8776c72c1a52cc3812057225154591aa9d
IMGP3DEV <42507604+Imgp3Dev@users.noreply.github.com>
2018-12-22 02:06:06 -0600

    Update MSR.d

--- MSR.d
+++ MSR.d
@@ -1,29 +1,111 @@
-// 'Encryption' Program.
-// Written in D
-/* MSR(Month shift right) Fun Program for
-ciphering.*/
-Month[string] = {"January", "Febuary",
-"March", "April", "May", "June", "July",
-"August", "September", "October",
-"November", "December"};
-//Month List
-EndLetters[char] = {'y', 'h', 'l', 'e',
-'t', 'r'};
-//Letter List
-writeln("Enter a Text: ");
-//Enter text
-writeln.log = "Input";
-//created "Input" assigned to be var for
-inputs.
+import std.stdio;
+import std.string;
+void main(){
+string Encrypt;
 
-if(check.EndLetters...%){
-wrintln();
-/*if it notices any of the endletters then
-it will just have it stay there and not
-change*/
-}else{
-input...%(Month)){
-check.Input(0);
-//Goes 1 to the right
-  writeln(Input);
-//Will be updated later.
+Encrypt[11]  Months = ["January", "Febuary", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
+writeln("Enter a string: ");
+str = readln;
+//January
+str.std.stdio("J", Encrypt[0,1]);
+str.std.stdio("a", Encrypt[0,2]);
+str.std.stdio("n", Encrypt[0,3]);
+str.std.stdio("u", Encrypt[0,4]);
+str.std.stdio("a", Encrypt[0,5]);
+str.std.stdio("r", Encrypt[0,6]);
+str.std.stdio("y", Encrypt[0,6]);
+ 
+//Febuary
+str.std.stdio("F", Encrypt[1,1
+str.std.stdio("e", Encrypt[1,2]);
+str.std.stdio("b", Encrypt[1,3]);
+str.std.stdio("u", Encrypt[1,4]);
+str.std.stdio("a", Encrypt[1,5]);
+str.std.stdio("r", Encrypt[1,6]);
+str.std.stdio("y", Encrypt[1,6]);
+//March
+str.std.stdio("M", Encrypt[2,1]);
+str.std.stdio("a", Encrypt[2,2]);
+str.std.stdio("r", Encrypt[2,3]);
+str.std.stdio("c", Encrypt[2,4]);
+str.std.stdio("h", Encrypt[2,4]);
+
+//April
+str.std.stdio("A", Encrypt[3,1]);
+str.std.stdio("p", Encrypt[3,2]);
+str.std.stdio("r", Encrypt[3,3]);
+str.std.stdio("i", Encrypt[3,5]);
+str.std.stdio("l", Encrypt[3,5]);
+
+//May
+str.std.stdio("M", Encrypt[4,1]);
+str.std.stdio("a", Encrypt[4,3]);
+str.std.stdio("y", Encrypt[4,3]);
+
+//June
+str.std.stdio("J", Encrypt[5,1]);
+str.std.stdio("u", Encrypt[5,2]);
+str.std.stdio("n", Encrypt[5,3]);
+str.std.stdio("e", Encrypt[5,3]);
+
+//July
+str.std.stdio("J", Encrypt[6,1]);
+str.std.stdio("u", Encrypt[6,2]);
+str.std.stdio("l", Encrypt[6,3]);
+str.std.stdio("y", Encrypt[6,3]);
+
+//August
+str.std.stdio("A", Encrypt[7,1]);
+str.std.stdio("u", Encrypt[7,2]);
+str.std.stdio("g", Encrypt[7,3]);
+str.std.stdio("u", Encrypt[7,4]);
+str.std.stdio("s", Encrypt[7,5]);
+str.std.stdio("t", Encrypt[7,5]);
+
+//September
+str.std.stdio("S", Encrypt[8,1]);
+str.std.stdio("e", Encrypt[8,2]);
+str.std.stdio("p", Encrypt[8,3]);
+str.std.stdio("t", Encrypt[8,4]);
+str.std.stdio("e", Encrypt[8,5]);
+str.std.stdio("m", Encrypt[8,6]);
+str.std.stdio("b", Encrypt[8,7]);
+str.std.stdio("e", Encrypt[8,8]);
+str.std.stdio("r", Encrypt[8,8]);
+
+//October
+str.std.stdio("O", Encrypt[9,1]);
+str.std.stdio("c", Encrypt[9,2]);
+str.std.stdio("t", Encrypt[9,3]);
+str.std.stdio("o", Encrypt[9,4]);
+str.std.stdio("b", Encrypt[9,5]);
+str.std.stdio("e", Encrypt[9,6]);
+str.std.stdio("r", Encrypt[9,6]);
+
+//November
+str.std.stdio("N", Encrypt[10,1]);
+str.std.stdio("o", Encrypt[10,2]);
+str.std.stdio("v", Encrypt[10,3]);
+str.std.stdio("e", Encrypt[10,4]);
+str.std.stdio("m", Encrypt[10,5]);
+str.std.stdio("b", Encrypt[10,6]);
+str.std.stdio("e", Encrypt[10,7]);
+str.std.stdio("r", Encrypt[10,7]);
+
+//December
+str.std.stdio("D", Encrypt[11,1]);
+str.std.stdio("e", Encrypt[11,2]);
+str.std.stdio("c", Encrypt[11,3]);
+str.std.stdio("e", Encrypt[11,4]);
+str.std.stdio("m", Encrypt[11,5]);
+str.std.stdio("b", Encrypt[11,6]);
+str.std.stdio("e", Encrypt[11,7]);
+str.std.stdio("r", Encrypt[11,7]);
+
+//None alphabet
+str.std.stdio("k", "k");
+str.std.stdio("q", "q");
+str.std.stdio("w", "w");
+str.std.stdio("x", "x");
+str.std.stdio("z", "z");
+}
